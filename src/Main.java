d5f8f596f78f1bad348b9e268b2a5b85b3fe0d23
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2024-11-24 14:21:32 +0530

    Added:  uc1-Check-employee

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+
+        final int IS_PRESENT = 1;
+
+        // Create a Random object
+        Random random = new Random();
+
+        // Generate random attendance (0 or 1)
+        int attendance = random.nextInt(2); // Generates 0 or 1
+
+        System.out.println("Employee Attendance Check:");
+
+        // Check if the employee is present or absent
+        if (attendance == IS_PRESENT) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
