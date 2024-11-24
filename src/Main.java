ed717240cc4f3679a822c152ac21c9715433999f
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2024-11-24 14:54:12 +0530

    Added : uc3-employee-part-time-wages

--- src/Main.java
+++ src/Main.java
@@ -7,26 +7,34 @@ public class Main {
         final int IS_PRESENT = 1;
         final int WAGE_PER_HOUR = 20;
         final int FULL_DAY_HOURS = 8;
+        final int PART_TIME_HOURS = 4;
+        final int IS_FULL_TIME = 1;
+        final int IS_PART_TIME = 2;
 
 
         // Create a Random object
         Random random = new Random();
 
         // Generate random attendance (0 or 1)
-        int attendance = random.nextInt(2); // Generates 0 or 1
+        int attendance = random.nextInt(3); // Generates 0 or 1 and 2
 
         System.out.println("Employee Attendance Check:");
 
         System.out.println("Employee Daily Wage Calculation:");
 
+        int dailyWage = 0;
         // Check if the employee is present or absent
-        if (attendance == IS_PRESENT) {
-            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
-            System.out.println("Employee is Present");
+        if (attendance == IS_FULL_TIME) {
+             dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
+            System.out.println("Employee is Full Time");
             System.out.println("Daily Wage: " + dailyWage);
-        } else {
-            System.out.println("Employee is Absent");
-            System.out.println("Daily Wage: 0");
+        } else if (attendance == IS_PART_TIME){
+            dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
+            System.out.println("Employee is Part tIME");
         }
+        else {
+            System.out.println("Employee is Absent");
+        }
+        System.out.println("Daily Wage: " + dailyWage);
     }
 }
