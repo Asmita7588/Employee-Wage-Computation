1f828d412a87485b9d96e2b65691fe1651d539d4
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2024-11-25 11:10:04 +0530

    Added : uc5-calculate-wage-till-maxhours

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,9 @@ public class Main {
         final int IS_FULL_TIME = 1;
         final int IS_PART_TIME = 2;
         final int WORKING_DAY_PER_MONTH = 20;
+        final int MAXIMUM_WORKING_HOURS = 100;
+        final int MAXIMUM_WORKING_DAYS = 20;
+
 
 
         // Create a Random object
@@ -23,31 +26,52 @@ public class Main {
 
         System.out.println("Employee Daily Wage Calculation:");
 
+        int totalWorkingHour = 0;
+        int totalWorkingDay = 0;
         int totalMonthlyWages = 0;
-        for(int day = 1; day<= WORKING_DAY_PER_MONTH; day++) {
+        while(totalWorkingHour < MAXIMUM_WORKING_HOURS && totalWorkingDay < MAXIMUM_WORKING_DAYS) {
 
             attendance = random.nextInt(3);
-            int dailyWage = 0;
-
+            int dailyHours = 0;
 
 
             switch (attendance) {
 
                 case IS_FULL_TIME:
-                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
-                    System.out.println("Day " +day );
-                    System.out.println("Daily Wage: " + dailyWage);
+                    dailyHours = FULL_DAY_HOURS;
+                    System.out.println("Day " +(totalWorkingDay +1)  );
+                    System.out.println("Full Time Hours Worked: " + dailyHours);
                     break;
                 case IS_PART_TIME:
-                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
-                    System.out.println("Employee is Part tIME");
+                    dailyHours = PART_TIME_HOURS;
+                    System.out.println("Day " + (totalWorkingDay +1) + "Part Time Hours Worked " +dailyHours);
                     break;
 
                 default:
-                    System.out.println("Employee is Absent");
+                    System.out.println(" Day "+(totalWorkingDay +1)+  "Employee is Absent hours worked: 0");
             }
+            totalWorkingHour +=dailyHours;
+
+            if (totalWorkingHour > MAXIMUM_WORKING_HOURS) {
+                dailyHours -= (totalWorkingHour - MAXIMUM_WORKING_HOURS);
+                totalWorkingHour = MAXIMUM_WORKING_HOURS;
+            }
+
+            // Calculate daily wage and add to the total wage
+            int dailyWage = WAGE_PER_HOUR * dailyHours;
             totalMonthlyWages += dailyWage;
+            totalMonthlyWages += dailyWage;
+
+
+
+
+            // Increment working days only if the employee worked
+            if (dailyHours > 0) {
+                totalWorkingDay++;
+            }
         }
-        System.out.println("Total Monthly Wages: " + totalMonthlyWages);
+        System.out.println("Total working hours " + totalWorkingHour);
+        System.out.println("Total working DAY " + totalWorkingDay);
+        System.out.println("Total Monthly Wages " + totalMonthlyWages);
     }
 }
