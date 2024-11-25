e5dee84fa138cd287793b5a01b3ef06b1d377967
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2024-11-25 10:31:11 +0530

    Added :  uc5-calculate-monthly-wages

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         final int PART_TIME_HOURS = 4;
         final int IS_FULL_TIME = 1;
         final int IS_PART_TIME = 2;
+        final int WORKING_DAY_PER_MONTH = 20;
 
 
         // Create a Random object
@@ -22,23 +23,31 @@ public class Main {
 
         System.out.println("Employee Daily Wage Calculation:");
 
-        int dailyWage = 0;
-        // Check if the employee is present or absent
-        switch (attendance ){
+        int totalMonthlyWages = 0;
+        for(int day = 1; day<= WORKING_DAY_PER_MONTH; day++) {
 
-            case  IS_FULL_TIME :
-                   dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
-                   System.out.println("Employee is Full Time");
-                   System.out.println("Daily Wage: " + dailyWage);
-                   break;
-            case  IS_PART_TIME:
-                        dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
-                        System.out.println("Employee is Part tIME");
-                        break;
+            attendance = random.nextInt(3);
+            int dailyWage = 0;
 
-        default:
-                 System.out.println("Employee is Absent");
+
+
+            switch (attendance) {
+
+                case IS_FULL_TIME:
+                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
+                    System.out.println("Day " +day );
+                    System.out.println("Daily Wage: " + dailyWage);
+                    break;
+                case IS_PART_TIME:
+                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
+                    System.out.println("Employee is Part tIME");
+                    break;
+
+                default:
+                    System.out.println("Employee is Absent");
+            }
+            totalMonthlyWages += dailyWage;
         }
-        System.out.println("Daily Wage: " + dailyWage);
+        System.out.println("Total Monthly Wages: " + totalMonthlyWages);
     }
 }
